d09cd7d51a3d655b4bc9f5eacb8fda2457347050
강혁준 <gurwns232@naver.com>
2023-03-31 03:50:46 +0900

    heart icon position click event

---
+++ app.js
@@ -0,0 +1,16 @@
+const image = document.querySelector('.img');
+const heart = document.querySelector('.heart');
+
+console.log(heart);
+
+image.addEventListener('dblclick', e => {
+    heart.style.left = `${e.clientX - e.target.offsetLeft}px`;
+    heart.style.top = `${e.clientY - e.target.offsetTop}px`;
+    console.log(heart.style.top);
+
+    heart.classList.add('active');
+
+    setTimeout(() => {
+        heart.classList.remove('active');
+    }, 1000);
+});
