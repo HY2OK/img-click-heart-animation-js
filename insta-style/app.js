0c966280eb4ce21a662b7052a2b497473a829cf3
강혁준 <gurwns232@naver.com>
2023-04-01 03:11:17 +0900

    좋아요 시 인원 추가 함수 추가

--- insta-style/app.js
+++ insta-style/app.js
@@ -1,11 +1,14 @@
 const $image = document.querySelector('.image');
 const $heart = document.querySelector('.heart');
+const $people = document.querySelector('.people');
 
 $image.addEventListener('dblclick', e => {
     $heart.style.top = `${e.clientY - e.target.offsetTop}px`;
     $heart.style.left = `${e.clientX - e.target.offsetLeft}px`;
 
     $heart.classList.add('active');
+
+    $people.innerText = Number($people.innerText) + 1;
 
     setTimeout(() => {
         $heart.classList.remove('active');
